2cab2232bd658f73b8ce51b6ed137e7b69fefdea
Zals <rahmataprizal403@gmail.com>
2024-05-09 07:35:03 +0700

    Zhubot.js

--- Zhubot.js
+++ Zhubot.js
@@ -2,6 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const commands = require("./libs/commands");
 const { helpTextMessage, invalidCommandMessage } = require("./libs/constant");
 const getGreeting = require("./app/getGreeting");
+const getQuake = require("./app/getQuake");
 
 class Zhubot extends TelegramBot {
     constructor(token, options) {
@@ -10,35 +11,55 @@ class Zhubot extends TelegramBot {
     }
 
     initializeBot() {
-        this.on("message", (data) => {
-            console.log(`Invalid Command Executed By ${data.from.username} => ${data.text}`);
-            const isInCommand = Object.values(commands).some(keyword => keyword.test(data.text));
+        this.on("message", (message) => {
+            const text = message.text ? message.text.trim() : '';
 
-            if (!isInCommand) {
-                this.sendMessage(data.from.id, invalidCommandMessage, {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [
-                                {
-                                    text: "Panduan Pengguna",
-                                    callback_data: "go_to_help"
-                                }
+            if (!text) return; // Memastikan pesan tidak kosong atau null
+
+            // Periksa apakah pesan dimulai dengan salah satu prefix yang valid
+            const isValidPrefix = Object.keys(commands).some(prefix => text.startsWith(prefix));
+
+            // Jika pesan dimulai dengan prefix yang valid, lanjutkan untuk memeriksa apakah itu adalah perintah yang valid
+            if (isValidPrefix) {
+                console.log(`Command Executed By ${message.from.username || message.from.first_name} => ${text}`);
+                
+                const isInCommand = Object.values(commands).some(keyword => keyword.test(text));
+
+                if (!isInCommand) {
+                    // Jika pesan dimulai dengan prefix yang valid tapi bukan perintah yang valid, kirim pesan bahwa perintah tidak valid
+                    const replyTo = message.chat.type === "group" ? message.chat.id : message.from.id;
+                    this.sendMessage(replyTo, invalidCommandMessage, {
+                        reply_markup: {
+                            inline_keyboard: [
+                                [
+                                    {
+                                        text: "Panduan Pengguna",
+                                        callback_data: "go_to_help"
+                                    }
+                                ]
                             ]
-                        ]
-                    }
-                });
+                        }
+                    });
+                }
+            } else {
+                // Jika pesan dimulai dengan karakter yang cocok dengan prefix tetapi tidak ada perintah, kirim pesan panduan pengguna
+                if (message.chat.type === "group") {
+                    this.sendMessage(message.chat.id, helpTextMessage);
+                }
             }
         });
 
         this.on("callback_query", (callback) => {
             const callbackName = callback.data;
-            if (callbackName == "go_to_help") {
+            if (callbackName === "go_to_help") {
                 this.sendMessage(callback.from.id, helpTextMessage);
             }
         });
 
         // Panggil setiap fitur terpisah di sini
         getGreeting(this, commands);
+        getTagAll(this, commands);
+        getQuake(this, commands);
         // Panggil fitur lainnya juga
     }
 }
