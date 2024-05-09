369cd1076ccaa9a9ed03a6c9ca78e0c4dca36e2c
Zals <rahmataprizal403@gmail.com>
2024-05-09 07:29:11 +0700

    commands.js

--- libs/commands.js
+++ libs/commands.js
@@ -2,7 +2,6 @@ const prefixes = ['!', '+', '#', '.', '/'];
 
 const commands = {
     greeting: new RegExp('^[' + prefixes.join('') + ']halo$'),
-    tagAll: new RegExp('^[' + prefixes.join('') + ']tagall$'),
     quake: new RegExp('^[' + prefixes.join('') + ']gempa$'),
     tourl: new RegExp('^[' + prefixes.join('') + ']tourl$')
 };
